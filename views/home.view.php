7357539c652463f842dc0b1b988b2a741141d15b
Ela-El-maker <feloela101@gmail.com>
2025-02-28 00:33:38 +0300

    Project initaialization

---
+++ views/home.view.php
@@ -0,0 +1,27 @@
+<?php loadPartial('header'); ?>
+
+<!-- Banner section start  -->
+<?php loadPartial('banner'); ?>
+<!-- Banner section end  -->
+
+<!-- work section start  -->
+<?php loadPartial('work'); ?>
+<!-- work section end -->
+
+<!-- feature section start  -->
+<?php loadPartial('featured'); ?>
+<!-- feature section end -->
+
+<!-- popular-loc section start  -->
+<?php loadPartial('popular-location'); ?>
+<!-- popular-loc section end -->
+
+<!-- recent-post section start  -->
+<?php loadPartial('recent-post'); ?>
+<!-- recent-post section end -->
+
+<!-- membership-call section start  -->
+<?php loadPartial('membership-call'); ?>
+<!-- membership-call section start  -->
+
+<?php loadPartial('footer'); ?>
---
+++ views/partials/membership-call.php
@@ -0,0 +1,15 @@
+<section class="section membership-call">
+    <div class="container">
+        <div class="membership-call__content" style="background: url('src/images/pattern/img-01.png') center center/cover no-repeat;">
+            <div class="membership-call__left">
+                <h2 class="text--heading-2 membership-call__title">Get Premium Membership</h2>
+                <p class="text--body-3 membership-call__description">
+                    Vestibulum consectetur placerat tellus. Sed faucibus fermentum purus, at facilisis neque auctor.
+                </p>
+            </div>
+            <div class="membership-call__right">
+                <a href="get-membership.html" class="btn btn--lg">Get membership</a>
+            </div>
+        </div>
+    </div>
+</section>
