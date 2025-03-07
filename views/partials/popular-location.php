37ae8e94c614aae2375aa318c8ec2fcab49291b4
Ela-El-maker <feloela101@gmail.com>
2025-03-07 13:49:59 +0300

    Created API Routes

---
+++ views/partials/popular-location.php
@@ -0,0 +1,185 @@
+<section class="section popular-location">
+    <div class="container">
+        <h2 class="text--heading-1 section__title">
+            Popular Location
+        </h2>
+        <div class="row">
+            <div class="col-xl-3 col-md-6">
+                <div class="location-card">
+                    <a href="ad-details.html" class="location-card__img-wrapper">
+                        <img src="./src/images/location/img-01.png" alt="location" />
+                    </a>
+                    <div class="location-card__info">
+                        <h2 class="location-card__loc-title text--body-2-600">Las Vegas, Usa</h2>
+                        <div class="location-card__view">
+                            <span class="first view-number">4,391</span>
+                            <a href="ad-details.html" class="second view-btn">
+                                View Ads
+                                <span class="icon">
+                                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M4.25 12H20.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M14 5.25L20.75 12L14 18.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+                <div class="location-card">
+                    <a href="ad-details.html" class="location-card__img-wrapper">
+                        <img src="./src/images/location/img-02.png" alt="location" />
+                    </a>
+                    <div class="location-card__info">
+                        <h2 class="location-card__loc-title text--body-2-600">New York, USA</h2>
+                        <div class="location-card__view">
+                            <span class="first view-number">4,391</span>
+                            <a href="ad-details.html" class="second view-btn">
+                                View Ads
+                                <span class="icon">
+                                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M4.25 12H20.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M14 5.25L20.75 12L14 18.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+                <div class="location-card">
+                    <a href="ad-details.html" class="location-card__img-wrapper">
+                        <img src="./src/images/location/img-03.png" alt="location" />
+                    </a>
+                    <div class="location-card__info">
+                        <h2 class="location-card__loc-title text--body-2-600">Mumbai, India</h2>
+                        <div class="location-card__view">
+                            <span class="first view-number">4,391</span>
+                            <a href="ad-details.html" class="second view-btn">
+                                View Ads
+                                <span class="icon">
+                                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M4.25 12H20.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M14 5.25L20.75 12L14 18.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+                <div class="location-card">
+                    <a href="ad-details.html" class="location-card__img-wrapper">
+                        <img src="./src/images/location/img-04.png" alt="location" />
+                    </a>
+                    <div class="location-card__info">
+                        <h2 class="location-card__loc-title text--body-2-600">Dublin, Ireland</h2>
+                        <div class="location-card__view">
+                            <span class="first view-number">4,391</span>
+                            <a href="ad-details.html" class="second view-btn">
+                                View Ads
+                                <span class="icon">
+                                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M4.25 12H20.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M14 5.25L20.75 12L14 18.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+                <div class="location-card">
+                    <a href="ad-details.html" class="location-card__img-wrapper">
+                        <img src="./src/images/location/img-05.png" alt="location" />
+                    </a>
+                    <div class="location-card__info">
+                        <h2 class="location-card__loc-title text--body-2-600">Manila, Philippines</h2>
+                        <div class="location-card__view">
+                            <span class="first view-number">4,391</span>
+                            <a href="ad-details.html" class="second view-btn">
+                                View Ads
+                                <span class="icon">
+                                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M4.25 12H20.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M14 5.25L20.75 12L14 18.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+                <div class="location-card">
+                    <a href="ad-details.html" class="location-card__img-wrapper">
+                        <img src="./src/images/location/img-06.png" alt="location" />
+                    </a>
+                    <div class="location-card__info">
+                        <h2 class="location-card__loc-title text--body-2-600">Bangkok, Thailand</h2>
+                        <div class="location-card__view">
+                            <span class="first view-number">4,391</span>
+                            <a href="ad-details.html" class="second view-btn">
+                                View Ads
+                                <span class="icon">
+                                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M4.25 12H20.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M14 5.25L20.75 12L14 18.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+                <div class="location-card">
+                    <a href="ad-details.html" class="location-card__img-wrapper">
+                        <img src="./src/images/location/img-07.png" alt="location" />
+                    </a>
+                    <div class="location-card__info">
+                        <h2 class="location-card__loc-title text--body-2-600">kuala Lumpur, Malaysia</h2>
+                        <div class="location-card__view">
+                            <span class="first view-number">4,391</span>
+                            <a href="ad-details.html" class="second view-btn">
+                                View Ads
+                                <span class="icon">
+                                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M4.25 12H20.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M14 5.25L20.75 12L14 18.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-md-6">
+                <div class="location-card">
+                    <a href="ad-details.html" class="location-card__img-wrapper">
+                        <img src="./src/images/location/img-08.png" alt="location" />
+                    </a>
+                    <div class="location-card__info">
+                        <h2 class="location-card__loc-title text--body-2-600">Jakarta, Indonesia</h2>
+                        <div class="location-card__view">
+                            <span class="first view-number">4,391</span>
+                            <a href="ad-details.html" class="second view-btn">
+                                View Ads
+                                <span class="icon">
+                                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M4.25 12H20.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                        <path d="M14 5.25L20.75 12L14 18.75" stroke="#00AAFF" stroke-width="1.6" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
